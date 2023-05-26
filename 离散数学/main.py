6de2364ccc147588412791ac99f6a3a97205fb33
dszfzzm <dszfzzm@163.com>
2023-05-26 12:39:04 +0800

    1

--- 离散数学/main.py
+++ 离散数学/main.py
@@ -1,8 +1,10 @@
+#定义边类，其中包含边的起始点及权值
 class Edge():
     def __init__(self,start:str,end:str,W:int):
         self.start=start
         self.end=end
         self.w=W
+#定义判断边存在函数，用于判断两点间是否存在边
 def isEdge(start:str,end:str):
     c = 0
     for e in edge:
@@ -12,15 +14,18 @@ def isEdge(start:str,end:str):
         else:
             c = 1
     return c
+#定义获取边长度函数，用于获取两点之间边的权值
 def lenth(start:str,end:str):
     for e in edge:
         if e.start == start and e.end == end:
             return e.w
     return 0
+#定义寻找对应节点函数，用于查找边权值对应的终点
 def find_num(matrix:list[list], num:int,end_point:int):
     for i in range(len(matrix)):
         if matrix[i][end_point]==num:
             return i
+#定义所要研究的图 包含两个列表 分别为节点和边
 vertiex=['a','b','c','d','e']
 e1=Edge('a','b',2)
 e2=Edge('a','d',10)
@@ -30,6 +35,7 @@ e5=Edge('c','d',6)
 e6=Edge('c','e',4)
 e7=Edge('e','d',5)
 edge=[e1,e2,e3,e4,e5,e6,e7]
+#利用狄杰斯特拉算法得出图的最短路矩阵
 T=['a']
 V=vertiex[:]
 result=[]
@@ -50,7 +56,7 @@ for i in vertiex:
         else:
             result[rcount].append(L+lenth(i,j))
     rcount+=1
-print(result)
+#将获得的矩阵进行变换，便于寻找最短路
 long=len(result)-1
 res=[]
 for i in range(long):
@@ -62,6 +68,7 @@ for i in range(long):
             res[i].append(float('inf'))
     res[i].reverse()
 temp=list(map(list,zip(*res)))
+#寻找a与各点间的距离和最短路
 for i in range(long):
     temp[i].sort()
     print('a与%s的距离为%d'%(vertiex[i+1],temp[i][0]))
@@ -70,6 +77,3 @@ for i in range(long):
     way.append(vertiex[i+1])
     print('路径为')
     print(way)
-
-
-
