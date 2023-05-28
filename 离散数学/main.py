7a35f9157720ed345f6b9aabaf2e80af5b374834
dszfzzm <dszfzzm@163.com>
2023-05-28 16:08:21 +0800

    1

--- 离散数学/main.py
+++ 离散数学/main.py
@@ -4,76 +4,47 @@ class Edge():
         self.start=start
         self.end=end
         self.w=W
-#定义判断边存在函数，用于判断两点间是否存在边
-def isEdge(start:str,end:str):
-    c = 0
-    for e in edge:
-        if e.start == start and e.end == end:
-            c = 0
-            break
-        else:
-            c = 1
-    return c
 #定义获取边长度函数，用于获取两点之间边的权值
 def lenth(start:str,end:str):
     for e in edge:
-        if e.start == start and e.end == end:
+        if (e.start == start and e.end == end) or (e.start == end and e.end == start):
             return e.w
-    return 0
-#定义寻找对应节点函数，用于查找边权值对应的终点
-def find_num(matrix:list[list], num:int,end_point:int):
-    for i in range(len(matrix)):
-        if matrix[i][end_point]==num:
-            return i
+    return float('inf')
 #定义所要研究的图 包含两个列表 分别为节点和边
-vertiex=['a','b','c','d','e']
+vertiex=['a','b','c','d','e','f']
 e1=Edge('a','b',2)
-e2=Edge('a','d',10)
+e2=Edge('a','d',5)
 e3=Edge('b','c',3)
-e4=Edge('b','d',7)
-e5=Edge('c','d',6)
-e6=Edge('c','e',4)
-e7=Edge('e','d',5)
-edge=[e1,e2,e3,e4,e5,e6,e7]
-#利用狄杰斯特拉算法得出图的最短路矩阵
+e4=Edge('b','d',1)
+e5=Edge('b','e',7)
+e6=Edge('c','d',1)
+e7=Edge('c','e',3)
+e8=Edge('c','f',5)
+e9=Edge('d','e',2)
+e10=Edge('e','f',1)
+edge=[e1,e2,e3,e4,e5,e6,e7,e8,e9,e10]
+#利用狄杰斯特拉算法得出图的最短路
 T=['a']
-V=vertiex[:]
-result=[]
-rcount=0
+V=vertiex[1:]
+result=[0]
+edgeLen=[]
 L=0
-for i in vertiex:
-    if isEdge(T[-1],i)==0:
-        T.append(i)
-    V.remove(i)
+for rcount in range(len(vertiex)-1):
     if len(T)==1:
         L=0
+        T.append(vertiex[1])
+        for i in V:
+            if lenth(T[0],i)<lenth(T[0],T[1]):
+                T[-1]=i
+        V.remove(T[-1])
     else:
-        L+=lenth(T[-2],T[-1])
-    result.append([])
-    for j in V:
-        if isEdge(i,j)!=0:
-            result[rcount].append(float('inf'))
-        else:
-            result[rcount].append(L+lenth(i,j))
-    rcount+=1
-#将获得的矩阵进行变换，便于寻找最短路
-long=len(result)-1
-res=[]
-for i in range(long):
-    res.append([])
-    for j in reversed(range(long)):
-        if j-i>-1:
-            res[i].append(result[i][j-i])
-        else:
-            res[i].append(float('inf'))
-    res[i].reverse()
-temp=list(map(list,zip(*res)))
-#寻找a与各点间的距离和最短路
-for i in range(long):
-    temp[i].sort()
-    print('a与%s的距离为%d'%(vertiex[i+1],temp[i][0]))
-    loc=find_num(res,temp[i][0],i)
-    way=vertiex[0:loc+1]
-    way.append(vertiex[i+1])
-    print('路径为')
-    print(way)
+        T.append(vertiex[rcount+1])
+        for i in V:
+            if lenth(T[-2],i)<lenth(T[-2],T[-1]):
+                T[-1]=i
+        V.remove(T[-1])
+    result.append(L+lenth(T[-2],T[-1]))
+    L+=lenth(T[-2],T[-1])
+for i in T:
+    print('a到'+i+'的距离为'+str(result[T.index(i)]))
+    print('路径为'+str(T[0:T.index(i)+1]))
